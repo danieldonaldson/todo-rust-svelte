74646667ff7a4f5853dcb7c804640c49b3beecce
daniel.donaldson <dandon08@gmail.com>
2023-07-28 19:56:49 +0200

    setup fe and be

---
+++ todo-frontend/src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<div class="md:container mx-auto mt-4 text-center">
+	<h1 class="text-3xl font-bold underline">Welcome to TODO app!</h1>
+	<p>Empty</p>
+</div>
+
+<style lang="postcss">
+	:global(html) {
+		background-color: theme(colors.gray.400);
+	}
+</style>
